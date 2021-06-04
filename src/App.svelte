796d4dcd54fff0039e89b220d97036ddedb0012b
Alve Larsson <larsalvelarsson@gmail.com>
2021-06-04 10:44:24 +0200

    Style setup

--- src/App.svelte
+++ src/App.svelte
@@ -2,5 +2,10 @@
 </script>
 
 <main>
+	<div class="titlebar"><p>Hello world!</p></div>
 	<h1>Crystal Editor</h1>
+	<img src="icons/crystalhook.svg" class="box" height="32" alt="">
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, ab vitae. Expedita repudiandae cumque atque aliquam exercitationem harum fugit unde eligendi amet rem praesentium qui, cupiditate, ullam quisquam earum distinctio.</p>
+	<hr>
+	<div class="code"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, ab vitae. Expedita repudiandae cumque atque aliquam exercitationem harum fugit unde eligendi amet rem praesentium qui, cupiditate, ullam quisquam earum distinctio.</p></div>
 </main>
