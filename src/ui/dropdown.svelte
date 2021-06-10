570c3c976b1ded43dc103dfa4e8858fb6fcccd6d
Alve Larsson <larsalvelarsson@gmail.com>
2021-06-10 05:25:37 +0200

    Update dropdown.svelte

--- src/ui/dropdown.svelte
+++ src/ui/dropdown.svelte
@@ -37,18 +37,9 @@
         box-shadow: 0px 6px 8px 0px rgba(0, 0, 0, 0.2);
         z-index: 1;
     }
-
-    .dropdown-content a {
-        color: black;
-        padding: 5px 5px;
-        text-decoration: none;
-        display: flex;
+    .dropdown:hover .dropbtn{
+        background-color: var(--ce-bg);
     }
-
-    .dropdown-content a:hover {
-        background-color: #f1f1f1;
-    }
-
     .dropdown:hover .dropdown-content {
         display: flex;
     }
