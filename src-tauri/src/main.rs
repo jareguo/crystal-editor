55a60ea5523467f0c66e0b5df88a201c842928d5
Alve Larsson <larsalvelarsson@gmail.com>
2021-09-20 05:24:19 +0200

    System tray + Icon compression

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,19 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 fn main() {
+
+  // Desktop tray
+  let exit_item = CustomMenuItem::new("exit".to_string(), "Exit");
+  let tray_menu = SystemTrayMenu::new().add_item(exit_item);
+  
+  let tray = SystemTray::new().with_menu(tray_menu);
+
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![])
+    .system_tray(tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
