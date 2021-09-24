c93bf4424d3138573b791b183aed2e23830af97b
Alve Larsson <larsalvelarsson@gmail.com>
2021-09-24 17:16:30 +0200

    Add System tray exit + Icon bug

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,11 +17,33 @@ fn main() {
   let exit_item = CustomMenuItem::new("exit".to_string(), "Exit");
   let tray_menu = SystemTrayMenu::new().add_item(exit_item);
   
-  let tray = SystemTray::new().with_menu(tray_menu);
+  // TODO: windows 11 not showing tray icon
+  let tray = SystemTray::new().with_menu(tray_menu).with_icon(tauri::Icon::Raw(include_bytes!("../icons/icon.png").to_vec()));
 
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![editor_instance_init])
     .system_tray(tray)
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::MenuItemClick { id, .. } => {
+        //let item_handle = app.tray_handle().get_item(&id);
+        match id.as_str() {
+          "exit" => {
+            let window = app.get_window("main").unwrap();
+            match window.close(){
+              Ok(_) => {
+                println!("Window closed");
+              },
+              Err(e) => {
+                println!("Error closing window: {}", e);
+              }
+            }
+            std::process::exit(0);
+          }
+          _ => {}
+        }
+      }
+      _ => {}
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
