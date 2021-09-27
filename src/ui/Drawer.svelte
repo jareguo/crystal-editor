01f114efed0fee0c857959935f31d30da209b660
Alve Larsson <larsalvelarsson@gmail.com>
2021-09-27 02:22:04 +0200

    Drawer improvement

--- src/ui/Drawer.svelte
+++ src/ui/Drawer.svelte
@@ -10,8 +10,8 @@
 <div class="drawer">
     <button class="flex row dbtn" on:click="{() => {triggerd = !triggerd}}">
         {lable}
-        <div style="margin-left: auto;">
-            <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div style="margin-left: auto; transition: 0.2s; transform: {triggerd ? 'rotate(0deg)' : 'rotate(90deg)'}" >
+            <svg width="12" height="12" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 3.6825H3.68248H7.36497L3.68248 7.36498L0 3.6825Z" fill="var(--fg)"/>
             </svg>                
         </div>
